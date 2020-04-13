5899c16cf67a81796531380d071b7e36ea54bb25
Travis CI User <travis@example.org>
2020-04-13 15:10:12 +0000

    Documentation for ndesh26/evdev-rs@03d7695

---
+++ implementors/core/convert/trait.TryFrom.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["evdev_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/std/time/struct.SystemTime.html\" title=\"struct std::time::SystemTime\">SystemTime</a>&gt; for <a class=\"struct\" href=\"evdev_rs/struct.TimeVal.html\" title=\"struct evdev_rs::TimeVal\">TimeVal</a>","synthetic":false,"types":["evdev_rs::TimeVal"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
